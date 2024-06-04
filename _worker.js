3917c87e63fe52068fbd302b990e1976b50b9a62
sadeqabbasi66 <123704474+sadeqabbasi66@users.noreply.github.com>
2024-06-04 05:46:10 +0330

    _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '089bce82-f11e-4f17-86e9-05a5829709d0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
